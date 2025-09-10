e74e4a9dfdf6e7a4ed47e64ef8ba50f9ca058a70
Ansh Mehta <anshkapilmehta@gmail.com>
2025-09-10 20:37:52 +0530

    feat: Implement tracker connection and file request logic

--- Client/leecher.ts
+++ Client/leecher.ts
@@ -1,3 +1,29 @@
-class Leecher {
+import { io, Socket } from "socket.io-client";
 
+class Leecher {
+    private trackerURL:string;
+    private trackerSocket:Socket;
+
+    constructor(trackerURL:string) {
+        this.trackerURL = trackerURL;
+        this.trackerSocket = io(trackerURL);
+
+        this.trackerSocket.on("connect", () => {
+            console.log("Connected to tracker at", trackerURL);
+        });
+
+        this.trackerSocket.on("disconnect", () => {
+            console.log("Disconnected from tracker at", trackerURL);
+        });
+
+        this.trackerSocket.on("error", (err) => {
+            console.error("Error with tracker connection:", err);
+        });
+    }
+
+    public requestFilesList():void {
+        this.trackerSocket.emit("requestFilesList");
+    }
 }
+
+const l : Leecher = new Leecher("http://localhost:3000");
