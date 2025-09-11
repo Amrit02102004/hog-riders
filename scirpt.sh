9546d1376f33c069d3f6a19824aa83d0f6cd9331
Amrit02102004 <amritsundarka@gmail.com>
2025-09-12 02:59:46 +0530

    script fix

--- scirpt.sh
+++ scirpt.sh
@@ -4,7 +4,6 @@ set -e  # exit if any command fails
 # Navigate to Web_Server
 echo "Building and starting Web_Server..."
 cd Web_Server
-npm install
 npm run build
 npm run start &   # run in background so frontend can also run
 cd ..
@@ -12,5 +11,4 @@ cd ..
 # Navigate to Frontend
 echo "Starting Frontend..."
 cd Frontend
-npm install
 npm run dev
