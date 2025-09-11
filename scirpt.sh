166e8ef25d09af04f085437ddd1753ca2aa78b68
Amrit02102004 <amritsundarka@gmail.com>
2025-09-12 02:55:43 +0530

    push the script

---
+++ scirpt.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e  # exit if any command fails
+
+# Navigate to Web_Server
+echo "Building and starting Web_Server..."
+cd Web_Server
+npm install
+npm run build
+npm run start &   # run in background so frontend can also run
+cd ..
+
+# Navigate to Frontend
+echo "Starting Frontend..."
+cd Frontend
+npm install
+npm run dev
